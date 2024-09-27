566627192f085cfcc08a567ace94df375524e65f
haden <80521474+thdefn@users.noreply.github.com>
2024-09-27 13:51:39 +0900

    chore : Initial commit

---
+++ image-inference-api/src/main/java/nota/inference/ImageInferenceApiApplication.java
@@ -0,0 +1,13 @@
+package nota.inference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageInferenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageInferenceApiApplication.class, args);
+	}
+
+}
---
+++ image-inference-api/src/test/java/nota/inference/ImageInferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nota.inference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageInferenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
